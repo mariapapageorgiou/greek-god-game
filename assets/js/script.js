04679dd3966622d71a4c4b4b62e9f12d9f83fa46
Maria Papageorgiou <mariageorgepapas@gmail.com>
2021-07-11 06:56:16 +0000

    Add basic functionality for rules modal

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// ===================================== RULES MODAL =====================================
+
+let modal = document.getElementById("rulesModal");
+let rulesBtn = document.getElementById("rulesBtn");
+
+rulesBtn.onclick = function() {
+    modal.style.display = "block";
+}
