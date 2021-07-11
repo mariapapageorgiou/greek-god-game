ce331f15851d1f4a648c83275a9e55a013e0ef32
Maria Papageorgiou <mariageorgepapas@gmail.com>
2021-07-11 07:24:11 +0000

    Add functionality and style for rules modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,15 @@ let rulesBtn = document.getElementById("rulesBtn");
 rulesBtn.onclick = function() {
     modal.style.display = "block";
 }
+
+let closeSpan = document.getElementsByClassName("close")[0];
+
+closeSpan.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
