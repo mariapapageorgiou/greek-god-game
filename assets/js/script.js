7726d473a5cb923c2467bfc7f8b90ce0ba785229
Maria Papageorgiou <mariageorgepapas@gmail.com>
2021-07-12 13:19:42 +0000

    Fix readability of code, water and fire elements selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,398 +19,45 @@ window.onclick = function (event) {
   }
 }
 
-// ===================================== GET ALL CHOICES BUTTONS =====================================
-
-let choicesDiv = document.getElementById("choices");
-let resultDiv = document.getElementById("result");
-
-// ==================== CATEGORY SPANS ====================
-let elementSpan = document.getElementById("categoryElement");
-let colorSpan = document.getElementById("categoryColor");
-let seasonSpan = document.getElementById("categorySeason");
-let senseSpan = document.getElementById("categorySense");
-let continentSpan = document.getElementById("categoryContinent");
-let animalSpan = document.getElementById("categoryAnimal");
-
-// ==================== BUTTONS IN CATEGORIES ====================
-
-// ==================== ELEMENTS ====================
-let waterBtn = document.getElementById("water");
-let fireBtn = document.getElementById("fire");
-
-// ==================== COLORS ====================
-let blueBtn = document.getElementById("blue");
-let yellowOneBtn = document.getElementById("yellow1");
-let yellowTwoBtn = document.getElementById("yellow2");
-let redBtn = document.getElementById("red");
-
-// ==================== SEASONS====================
-let winterBtn = document.getElementById("winter");
-let springOneBtn = document.getElementById("spring1");
-let springTwoBtn = document.getElementById("spring2");
-let summerOneBtn = document.getElementById("summer1");
-let summerTwoBtn = document.getElementById("summer2");
-let fallBtn = document.getElementById("fall");
-
-// ==================== SENSES ====================
-let sightBtn = document.getElementById("sight");
-let soundOneBtn = document.getElementById("sound1");
-let soundTwoBtn = document.getElementById("sound2");
-let smellOneBtn = document.getElementById("smell1");
-let smellTwoBtn = document.getElementById("smell2");
-let tasteOneBtn = document.getElementById("taste1");
-let tasteTwoBtn = document.getElementById("taste2");
-let touchBtn = document.getElementById("touch");
-
-// ==================== CONTINENTS ====================
-let antarcticaBtn = document.getElementById("antarctica");
-let oceaniaOneBtn = document.getElementById("oceania1");
-let oceaniaTwoBtn = document.getElementById("oceania2");
-let asiaOneBtn = document.getElementById("asia1");
-let asiaTwoBtn = document.getElementById("asia2");
-let europeOneBtn = document.getElementById("europe1");
-let europeTwoBtn = document.getElementById("europe2");
-let americasOneBtn = document.getElementById("americas1");
-let americasTwoBtn = document.getElementById("americas2");
-let africaBtn = document.getElementById("africa");
-
-// ==================== ANIMALS ====================
-let dolphin = document.getElementById("dolphin");
-let stork = document.getElementById("stork");
-let eagle = document.getElementById("eagle");
-let boar = document.getElementById("boar");
-let mouse = document.getElementById("mouse");
-let peacock = document.getElementById("peacock");
-let owl = document.getElementById("owl");
-let pig = document.getElementById("pig");
-let horse = document.getElementById("horse");
-let swan = document.getElementById("swan");
-let serpent = document.getElementById("serpent");
-let donkey = document.getElementById("donkey");
-
-// ==================== ELEMENT EVENT ====================
-waterBtn.onclick = function () {
-  waterBtn.style.display = "none";
-  fireBtn.style.display = "none";
-  elementSpan.style.display = "none";
-  blueBtn.style.display = "block";
-  yellowTwoBtn.style.display = "block";
-  colorSpan.style.display = "inline";
+// ===================================== FUNCTION FOR CHANGING THE DISPLAY =====================================
+function changeDisplay(changeElements) {
+  setAllElementsNone();
+  for (const property in changeElements) {
+    setEvent(property, changeElements[property])
+  };
 }
 
-fireBtn.onclick = function () {
-  waterBtn.style.display = "none";
-  fireBtn.style.display = "none";
-  elementSpan.style.display = "none";
-  redBtn.style.display = "block";
-  yellowOneBtn.style.display = "block";
-  colorSpan.style.display = "inline";
+// ===================================== FUNCTION TO SET ALL REQUIRED ELEMENTS TO NONE =====================================
+function setAllElementsNone() {
+  const allElements = ["water", "fire", "blue", "yellow1", "yellow2", "red", "winter", "spring1", "spring2", "summer1", "summer2", "fall", "sight", "sound1", "sound2", "smell1", "smell2", "taste1", "taste2", "touch", "antarctica", "oceania1", "oceania2", "asia1", "asia2", "europe1", "europe2", "americas1", "americas2", "africa", "dolphin", "stork", "eagle", "boar", "mouse", "peacock", "owl", "pig", "horse", "swan", "serpent", "donkey"];
+  allElements.forEach(function (eachElement) {
+    const elementToset = document.getElementById(eachElement);
+    elementToset.style.display = "none";
+  });
 }
 
-// ==================== COLOR EVENT ====================
-blueBtn.onclick = function () {
-  blueBtn.style.display = "none";
-  yellowTwoBtn.style.display = "none";
-  colorSpan.style.display = "none";
-  winterBtn.style.display = "block";
-  springTwoBtn.style.display = "block";
-  seasonSpan.style.display = "inline";
+// ===================================== FUNCTION TO SET THE EVENT =====================================
+function setEvent(eventName, setValue) {
+  const elementToset = document.getElementById(eventName);
+  elementToset.style.display = setValue;
 }
 
-function yellow() {
-  blueBtn.style.display = "none";
-  redBtn.style.display = "none";
-  yellowOneBtn.style.display = "none";
-  yellowTwoBtn.style.display = "none";
-  colorSpan.style.display = "none";
-  springOneBtn.style.display = "block";
-  summerTwoBtn.style.display = "block";
-  seasonSpan.style.display = "inline";
+// ===================================== WATER BUTTON EVENT =====================================
+water.onclick = function () {
+  let changeElements = {
+    blue: "block",
+    yellow2: "block",
+    categoryColor: "inline"
+  };
+  changeDisplay(changeElements);
 }
 
-yellowOneBtn.addEventListener('click', yellow);
-yellowTwoBtn.addEventListener('click', yellow);
-
-redBtn.onclick = function () {
-  redBtn.style.display = "none";
-  yellowOneBtn.style.display = "none";
-  colorSpan.style.display = "none";
-  summerOneBtn.style.display = "block";
-  fallBtn.style.display = "block";
-  seasonSpan.style.display = "inline";
+// ===================================== FIRE BUTTON EVENT =====================================
+fire.onclick = function () {
+  let changeElements = {
+    red: "block",
+    yellow1: "block",
+    categoryColor: "inline"
+  };
+  changeDisplay(changeElements);
 }
-
-// ==================== SEASON EVENT ====================
-winterBtn.onclick = function () {
-  winterBtn.style.display = "none";
-  springTwoBtn.style.display = "none";
-  seasonSpan.style.display = "none";
-  sightBtn.style.display = "block";
-  soundTwoBtn.style.display = "block";
-  senseSpan.style.display = "inline";
-}
-
-function spring() {
-  winterBtn.style.display = "none";
-  springTwoBtn.style.display = "none";
-  springOneBtn.style.display = "none";
-  summerTwoBtn.style.display = "none";
-  seasonSpan.style.display = "none";
-  soundOneBtn.style.display = "block";
-  smellTwoBtn.style.display = "block";
-  senseSpan.style.display = "inline";
-}
-
-springOneBtn.addEventListener('click', spring);
-springTwoBtn.addEventListener('click', spring);
-
-function summer() {
-  springOneBtn.style.display = "none";
-  summerTwoBtn.style.display = "none";
-  summerOneBtn.style.display = "none";
-  fallBtn.style.display = "none";
-  seasonSpan.style.display = "none";
-  smellOneBtn.style.display = "block";
-  tasteTwoBtn.style.display = "block";
-  senseSpan.style.display = "inline";
-}
-
-summerOneBtn.addEventListener('click', summer);
-summerTwoBtn.addEventListener('click', summer);
-
-fallBtn.onclick = function () {
-  summerOneBtn.style.display = "none";
-  fallBtn.style.display = "none";
-  seasonSpan.style.display = "none";
-  tasteOneBtn.style.display = "block";
-  touchBtn.style.display = "block";
-  senseSpan.style.display = "inline";
-}
-
-// ==================== SENSE EVENT ====================
-sightBtn.onclick = function () {
-  sightBtn.style.display = "none";
-  soundTwoBtn.style.display = "none";
-  senseSpan.style.display = "none";
-  antarcticaBtn.style.display = "block";
-  oceaniaTwoBtn.style.display = "block";
-  continentSpan.style.display = "inline";
-}
-
-function sound() {
-  sightBtn.style.display = "none";
-  soundTwoBtn.style.display = "none";
-  soundOneBtn.style.display = "none";
-  smellTwoBtn.style.display = "none";
-  senseSpan.style.display = "none";
-  oceaniaOneBtn.style.display = "block";
-  asiaTwoBtn.style.display = "block";
-  continentSpan.style.display = "inline";
-}
-
-soundOneBtn.addEventListener('click', sound);
-soundTwoBtn.addEventListener('click', sound);
-
-function smell() {
-  soundOneBtn.style.display = "none";
-  smellTwoBtn.style.display = "none";
-  smellOneBtn.style.display = "none";
-  tasteTwoBtn.style.display = "none";
-  senseSpan.style.display = "none";
-  asiaOneBtn.style.display = "block";
-  europeTwoBtn.style.display = "block";
-  continentSpan.style.display = "inline";
-}
-
-smellOneBtn.addEventListener('click', smell);
-smellTwoBtn.addEventListener('click', smell);
-
-function taste() {
-  smellOneBtn.style.display = "none";
-  tasteTwoBtn.style.display = "none";
-  tasteOneBtn.style.display = "none";
-  touchBtn.style.display = "none";
-  senseSpan.style.display = "none";
-  europeOneBtn.style.display = "block";
-  americasTwoBtn.style.display = "block";
-  continentSpan.style.display = "inline";
-}
-
-tasteOneBtn.addEventListener('click', taste);
-tasteTwoBtn.addEventListener('click', taste);
-
-touchBtn.onclick = function () {
-  tasteOneBtn.style.display = "none";
-  touchBtn.style.display = "none";
-  senseSpan.style.display = "none";
-  americasOneBtn.style.display = "block";
-  africaBtn.style.display = "block";
-  continentSpan.style.display = "inline";
-}
-
-// ==================== CONTINENT EVENT ====================
-antarcticaBtn.onclick = function () {
-  antarcticaBtn.style.display = "none";
-  oceaniaTwoBtn.style.display = "none";
-  continentSpan.style.display = "none";
-  dolphin.style.display = "block";
-  stork.style.display = "block";
-  animalSpan.style.display = "inline";
-}
-
-function oceania() {
-  antarcticaBtn.style.display = "none";
-  oceaniaTwoBtn.style.display = "none";
-  oceaniaOneBtn.style.display = "none";
-  asiaTwoBtn.style.display = "none";
-  continentSpan.style.display = "none";
-  eagle.style.display = "block";
-  boar.style.display = "block";
-  animalSpan.style.display = "inline";
-}
-
-oceaniaOneBtn.addEventListener('click', oceania);
-oceaniaTwoBtn.addEventListener('click', oceania);
-
-function asia() {
-  oceaniaOneBtn.style.display = "none";
-  asiaTwoBtn.style.display = "none";
-  asiaOneBtn.style.display = "none";
-  europeTwoBtn.style.display = "none";
-  continentSpan.style.display = "none";
-  mouse.style.display = "block";
-  peacock.style.display = "block";
-  animalSpan.style.display = "inline";
-}
-
-asiaOneBtn.addEventListener('click', asia);
-asiaTwoBtn.addEventListener('click', asia);
-
-function europe() {
-  asiaOneBtn.style.display = "none";
-  europeTwoBtn.style.display = "none";
-  europeOneBtn.style.display = "none";
-  americasTwoBtn.style.display = "none";
-  continentSpan.style.display = "none";
-  owl.style.display = "block";
-  pig.style.display = "block";
-  animalSpan.style.display = "inline";
-}
-
-europeOneBtn.addEventListener('click', europe);
-europeTwoBtn.addEventListener('click', europe);
-
-function americas() {
-  europeOneBtn.style.display = "none";
-  americasTwoBtn.style.display = "none";
-  americasOneBtn.style.display = "none";
-  africaBtn.style.display = "none";
-  continentSpan.style.display = "none";
-  horse.style.display = "block";
-  swan.style.display = "block";
-  animalSpan.style.display = "inline";
-}
-
-americasOneBtn.addEventListener('click', americas);
-americasTwoBtn.addEventListener('click', americas);
-
-africaBtn.onclick = function () {
-  americasOneBtn.style.display = "none";
-  africaBtn.style.display = "none";
-  continentSpan.style.display = "none";
-  serpent.style.display = "block";
-  donkey.style.display = "block";
-  animalSpan.style.display = "inline";
-}
-
-// ==================== GOD RESULT ====================
-
-let poseidon = document.getElementById("poseidon");
-let hermes = document.getElementById("hermes");
-let zeus = document.getElementById("zeus");
-let ares = document.getElementById("ares");
-let apollo = document.getElementById("apollo");
-let hera = document.getElementById("hera");
-let athena = document.getElementById("athena");
-let demeter = document.getElementById("demeter");
-let artemis = document.getElementById("artemis");
-let aphrodite = document.getElementById("aphrodite");
-let hades = document.getElementById("hades");
-let hephaestus = document.getElementById("hephaestus");
-
-function godResult() {
-  choicesDiv.style.display = "none";
-  resultDiv.style.display = "block";
-}
-
-function godPoseidon() {
-  godResult();
-  poseidon.style.display = "block";
-}
-dolphin.addEventListener('click', godPoseidon);
-
-function godHermes() {
-  godResult();
-  hermes.style.display = "block";
-}
-stork.addEventListener('click', godHermes);
-
-function godZeus() {
-  godResult();
-  zeus.style.display = "block";
-}
-eagle.addEventListener('click', godZeus);
-
-function godAres() {
-  godResult();
-  ares.style.display = "block";
-}
-boar.addEventListener('click', godAres);
-
-function godApollo() {
-  godResult();
-  apollo.style.display = "block";
-}
-mouse.addEventListener('click', godApollo);
-
-function godHera() {
-  godResult();
-  hera.style.display = "block";
-}
-peacock.addEventListener('click', godHera);
-
-function godAthena() {
-  godResult();
-  athena.style.display = "block";
-}
-owl.addEventListener('click', godAthena);
-
-function godDemeter() {
-  godResult();
-  demeter.style.display = "block";
-}
-pig.addEventListener('click', godDemeter);
-
-function godArtemis() {
-  godResult();
-  artemis.style.display = "block";
-}
-horse.addEventListener('click', godArtemis);
-
-function godAphrodite() {
-  godResult();
-  aphrodite.style.display = "block";
-}
-swan.addEventListener('click', godAphrodite);
-
-function godHades() {
-  godResult();
-  hades.style.display = "block";
-}
-serpent.addEventListener('click', godHades);
-
-function godHephaestus() {
-  godResult();
-  hephaestus.style.display = "block";
-}
-donkey.addEventListener('click', godHephaestus);
