4c02a0f687822d7be88c12716dcc1cc79709f293
Maria Papageorgiou <mariageorgepapas@gmail.com>
2021-07-12 00:30:38 +0000

    Add script for element choice spans

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,6 +21,9 @@ window.onclick = function (event) {
 
 // ===================================== GET ALL CHOICES BUTTONS =====================================
 
+let elementSpan = document.getElementById("categoryElement");
+let colorSpan = document.getElementById("categoryColor");
+
 let waterBtn = document.getElementById("water");
 let fireBtn = document.getElementById("fire");
 let blueBtn = document.getElementById("blue");
@@ -31,13 +34,17 @@ let redBtn = document.getElementById("red");
 waterBtn.onclick = function () {
   waterBtn.style.display = "none";
   fireBtn.style.display = "none";
+  elementSpan.style.display = "none";
   blueBtn.style.display = "block";
   yellowTwoBtn.style.display = "block";
+  colorSpan.style.display = "inline";
 }
 
 fireBtn.onclick = function () {
   waterBtn.style.display = "none";
   fireBtn.style.display = "none";
+  elementSpan.style.display = "none";
   redBtn.style.display = "block";
   yellowOneBtn.style.display = "block";
+  colorSpan.style.display = "inline";
 }
